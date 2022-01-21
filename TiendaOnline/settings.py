8e5d271838da4722e0b084bbccfe16509db262c0
John <88859219+Johnher2@users.noreply.github.com>
2022-01-21 17:26:26 -0500

    Borrar c

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -134,6 +134,6 @@ EMAIL_HOST ="smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT =587
 EMAIL_HOST_USER="aplicacionsalud1@gmail.com"
-EMAIL_HOST_PASSWORD="aplicacion123"
+EMAIL_HOST_PASSWORD=""
 
 # Revisar la documentacion python
